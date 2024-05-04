4022d6a635fe5a434ff584f272d12a3e5a6b0372
Arunkumar <arunkumar.msg1982@gmail.com>
2024-05-05 02:55:42 +0530

    add score

--- numGuessGame.js
+++ numGuessGame.js
@@ -1,35 +1,42 @@
 let answer = Math.floor((Math.random() * 20)+1);
-let guess = 10;
+let guess = 9;
 let message = document.getElementById("check")
 let displayScore = document.getElementById("score")
 let displayAnswer = document.getElementById("number")
+let restartGame = document.getElementById("againBtn") 
+restartGame.disabled = true
 function result() {
+    
     let userInput = (document.getElementById('userInput').value)
     if (!userInput) {
         message.textContent = "please enter the valid input"
     }
     else if (guess != 0) {
         if (userInput > answer) {
-            message.textContent = "your answer is high"
-            guess--
+            message.textContent = "your answer is high"            
             displayScore.textContent = guess
+            guess--
         }
         else if (userInput < answer) {
-            message.textContent = "your answer is low"
-            guess--
+            message.textContent = "your answer is low"            
             displayScore.textContent = guess
+            guess--
         }
         else if(userInput == answer) {
             message.textContent = " your are the win"
             displayScore.textContent = guess
             document.body.style.backgroundColor = "green";
             displayAnswer.textContent = answer;
+            restartGame.disabled = false
         }        
     }
     else{
+        
+        displayScore.textContent = guess
         message.textContent = "your are lose the game";
         document.body.style.backgroundColor = "red";
         displayAnswer.textContent = answer;
+        restartGame.disabled = false
     }
 }
 function reset(){
@@ -40,4 +47,6 @@ function reset(){
     message.textContent = "Start guessing..."
     displayScore.textContent = guess
     userInput.value = null
+    restartGame.disabled = true
+    guess--
 }
