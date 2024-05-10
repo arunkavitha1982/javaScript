105d125a637ddc9ef91ed228815b17fd3c8768ec
Arunkumar <arunkumar.msg1982@gmail.com>
2024-05-10 23:39:18 +0530

    new update

--- numGuessGame.js
+++ numGuessGame.js
@@ -6,7 +6,7 @@ let displayScore = document.getElementById("attemptcounter");
 let checkBoxInput = document.getElementById("number");
 let guesscounter = document.getElementById("guesscounter");
 let win = document.getElementById("scorecounter");
-let displayImage = document.getElementById("image");
+let displayImage = document.getElementById("result");
 let displayAnswer = document.getElementById("nomoreattempts");
 document.getElementById("other").style.display = "none";
 console.log(answer);
@@ -18,44 +18,32 @@ function result() {
     }
     else if (guess != 0) {
         if (userInput > answer) {
-            displayImage.src = "image/plane.webp";
-            displayImage.style.height = "80%";
-            displayImage.style.width = "90%";
-            displayImage.style.paddingLeft = "6%";
-            displayImage.style.paddingTop = "10%";
+            displayImage.style.backgroundImage = "url(image/plane.webp)";
             message.textContent = "Too High...Guess Lower";
             displayScore.textContent = guess + "/15";
             guess--;
             guesscounter.textContent += " " + userInput + ",";
             userInput.value = null;
-            
+
         }
         else if (userInput < answer) {
-            displayImage.src = "image/yellow.webp";
-            displayImage.style.height = "80%";
-            displayImage.style.width = "90%";
-            displayImage.style.paddingLeft = "6%";
-            displayImage.style.paddingTop = "10%";  
+            displayImage.style.backgroundImage ="url(image/yellow.webp)";
             message.textContent = "Too Low...Guess Higher";
             displayScore.textContent = guess + "/15";
             guesscounter.textContent += " " + userInput + ",";
             guess--;
-            userInput.value = null; 
-                 
+            userInput.value = null;
+
         }
         else {
             message.textContent = "Well Done. You Correctly Guessed That The Number Was " + answer + " . Try Another Number.";
             displayScore.textContent = guess;
             guesscounter.textContent += " " + userInput + ",";
             displayScore.textContent = guess + "/15";
-            displayImage.src = 'image/winners-trophy-clip-art.jpg'
-            displayImage.style.height = "80%";
-            displayImage.style.width = "90%";
-            displayImage.style.paddingLeft = "6%";
-            displayImage.style.paddingTop = "10%";
+            displayImage.style.backgroundImage = 'url(image/winners-trophy-clip-art.jpg)'
             winner++;
-            win.textContent = winner;
-            document.getElementById("streakcounter").textContent = guess;
+            win.textContent = winner+1;
+            document.getElementById("streakcounter").textContent = guess+1;
             document.getElementById("other").style.display = "block";
             document.getElementById("submit").style.display = "none";
             document.getElementById('guess').style.display = "none";
@@ -65,11 +53,7 @@ function result() {
     else {
         displayAnswer.textContent = "You Ran Out Of Attempts. The Number Was " + answer + ". Try Another Number.";
         displayAnswer.style.backgroundColor = "red";
-        displayImage.src = "image/loserImage.avif";
-        displayImage.style.height = "80%";
-        displayImage.style.width = "90%";
-        displayImage.style.paddingLeft = "6%";
-        displayImage.style.paddingTop = "10%";
+        displayImage.style.backgroundImage = "url(image/loserImage.avif)";
         displayScore.textContent = guess;
         message.textContent = "your are lose the game";
         document.getElementById("other").style.display = "block";
@@ -92,8 +76,7 @@ function reset() {
     guesscounter.textContent = "";
     message.textContent = "Try To Guess The Whole Number Between 0 And 20.";
     console.log(answer);
-    displayImage.src = "image/question-mark.webp";
-    
+    displayImage.style.backgroundImage = "url(image/question-mark.webp)";
     document.getElementById('guess').value = null;
     guess--;
     document.getElementById("other").style.display = "none";
@@ -102,6 +85,7 @@ function reset() {
     document.getElementById('giveup').style.display = "block";
     displayAnswer.textContent = "";
     displayAnswer.style.backgroundColor = "orange";
+    win.textContent = winner;
 }
 function giveup() {
     document.getElementById("other").style.display = "block";
