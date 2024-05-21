f3af1632f92defe0583a38042f82081b5776c70d
Arunkumar <arunkumar.msg1982@gmail.com>
2024-05-21 21:48:15 +0530

    Small changes

--- calculator.js
+++ calculator.js
@@ -1,9 +1,8 @@
 let display = document.getElementById('display');
 let dotClickable = true;
-
 function appendToDisplay(value) {
-    if(value==='.'){
-        display.textContent+=value
+    if (value === '.') {
+        display.textContent += value
     }
     else if (display.textContent === '0' || display.textContent === 'Error') {
         display.textContent = value;
@@ -12,44 +11,36 @@ function appendToDisplay(value) {
         display.textContent += value;
     }
 }
-
 function cancel() {
     display.textContent = 0;
     dotClickable = true;
 }
-
 function root() {
     let userInput = display.textContent;
     let result = Math.sqrt(userInput);
     display.textContent = result;
 }
-function squar(){
+function squar() {
     let userInput = display.textContent;
-    userInput =userInput*userInput;
-    display.textContent= userInput
+    userInput = userInput * userInput;
+    display.textContent = userInput
 }
 function deleteLast() {
     let text = display.textContent;
-    if (text.length > 1) {
+    if (text === 'cos(' || display.textContent === 'sin(' || display.textContent === 'tag(' || display.textContent === 'log(') {
+        display.textContent = '0';
+    } else if (text.length > 1) {
         display.textContent = text.slice(0, -1);
     } else {
         display.textContent = '0';
     }
 }
-
-
-function onDigit(value) {
-    appendToDisplay(value);
-}
-
-
 function dot() {
     if (dotClickable) {
         appendToDisplay('.');
         dotClickable = false;
     }
 }
-
 function operator(value) {
     appendToDisplay(value);
     dotClickable = true;
